eed43c934299365505ff1807b1ddcb57baa0f202
Hayk Khachatryan <kh.ha.hayk@gmail.com>
2019-10-23 16:44:14 +0100

    correct os.environ.get

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ class Config(object):
     DEBUG = False
     Testing = False
     CSRF_ENABLED = True
-    SECRET_KEY = os.environ('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class ProductionConfig(Config):
