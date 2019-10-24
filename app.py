ec64d20f73d61db6edb4e7e9ef84b0a85cfde39e
Hayk Khachatryan <kh.ha.hayk@gmail.com>
2019-10-24 23:30:06 +0100

    Rudimentary app.py prototype     * new type for SQLAlchemy     * added brains.py that checks IKEA API     * if there is an update today push to front end

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from typing import NewType
 import os
 
 app = Flask(__name__)
@@ -9,11 +10,20 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+Database = NewType('SQL', SQLAlchemy) 
+
 from models import Plant
+from brains import find_the_plants, update_today
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    find_the_plants("https://w53515517.api.esales.apptus.cloud/api/v1/panels/category-products-color?market=GBEN&arg.market=GBEN&arg.selected_category=category_catalog_gben:%2710779%27&arg.window_first=1&arg.window_last=1000", db)
+
+    if update_today():
+        return render_template("update.html")
+    else:
+        return render_template("no_update.html")
+
 
 if __name__ == '__main__':
     app.run()
