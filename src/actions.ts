234856719b0dfc57c3f221e74cdca465faccae4b
Hayk Khachatryan <hi@hayk.io>
2020-05-20 20:07:54 +0400

    Actions

---
+++ src/actions.ts
@@ -0,0 +1,50 @@
+/**
+ * @fileoverview state updating actions
+ *
+ * @author       Hayk Khachatryan
+ *
+ * @requires     types.ts
+ */
+
+import { State, Plant } from './types'
+
+/**
+ * Updates state.recents
+ *
+ * @param {State} state   system state
+ * @param {Plant[]} recents new recents value
+ *
+ * @returns {State} updated state
+ */
+const SetRecents = (state: State, recents: Plant[]): State => ({
+  ...state,
+  recents
+})
+
+/**
+ * Updates state.fetching
+ *
+ * @param {State} state      system state
+ * @param {boolean} fetching new fetching value
+ *
+ * @returns {State}          updated state
+ */
+const SetFetching = (state: State, fetching: boolean): State => ({
+  ...state,
+  fetching
+})
+
+/**
+ * Updates state.updateToday
+ *
+ * @param {State} state         system state
+ * @param {boolean} updateToday new updateToday value
+ *
+ * @returns {State}             updated state
+ */
+const SetUpdateToday = (state: State, updateToday: boolean): State => ({
+  ...state,
+  updateToday
+})
+
+export { SetRecents, SetFetching, SetUpdateToday }
